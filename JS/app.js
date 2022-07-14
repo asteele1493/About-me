4a5a865cf805ca20c679a297eb7d0b37335dfd11
Andra <94204279+asteele1493@users.noreply.github.com>
2022-07-13 21:08:14 -0700

    Added more js prompts and styling. Not done yet.

---
+++ JS/app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let answer1 = prompt('What\'s my favorite food?');
+console.log(answer1);
+
+if (answer1 === "Pizza"); {
+    alert('Heck yes, I fuck with pizza!');
+} else if (answer1 === 'Breakfast tacos') {
+    alert('Papas con huevo all day.');
+} else {
+    alert('Nah brah, not a fan.');
+}
+
+let answer2 = prompt('Where did I like living most?');
+console.log(answer2);
+
+if (answer2 === 'Houston') {
+    alert('Correct! H-town vicious baybee!');
+} else if (answer2 === 'Los Angeles') {
+    alert('You don\'t know me at all.');
+// } else {
+        alert('Are you serious?');
+}
