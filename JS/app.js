75e714eabeaa7b062b54884821948ebd4c52de22
Andra <94204279+asteele1493@users.noreply.github.com>
2022-07-18 22:02:29 -0700

    lab 3 mods

--- JS/app.js
+++ JS/app.js
@@ -10,7 +10,7 @@ answer1 = answer1.toLowerCase();
 
 if (answer1 === 'pizza') {
   window.alert('Heck yes I will literally eat pizza all day!');
-} else if (answer1 === 'Breakfast tacos') {
+} else if (answer1 === 'breakfast tacos') {
   window.alert('Papas con huevo is forever number 1 in my heart.');
 } else {
   window.alert('Nah brah, not a fan.');
@@ -22,11 +22,11 @@ console.log(answer2);
 answer2 = answer2.toLowerCase();
 
 if (answer2 === 'houston') {
-    alert('Correct! H-town vicious baybee!');
-} else if (answer2 === 'Los Angeles') {
-    alert('You don\'t know me at all.');
- } else {
-        alert('I guess that\'s cool too.');
+  alert('Correct! H-town vicious baybee!');
+} else if (answer2 === 'los angeles') {
+  alert('You don\'t know me at all.');
+} else {
+  alert('I guess that\'s cool too.');
 }
 
 let answer3 = prompt('What do I like doing with my free time?');
@@ -37,14 +37,13 @@ console.log(answer3);
 
 answer3 = answer3.toLowerCase();
 
-if (answer3 === 'Checking out new wine bars') {
-    alert('Absolutely! All about that natty life.');
-} else if (answer3 === 'Going to the beach.') {
-    alert('I do love some beach time');
+if (answer3 === 'checking out new wine bars') {
+  alert('Absolutely! All about that natty life.');
+} else if (answer3 === 'going to the beach.') {
+  alert('I do love some beach time');
 } else {
-    alert('Did you read my bio? :)');
+  alert('Did you read my bio? :)');
 }
-
 
 let answer4 = prompt('Do I like tinned conservas?');
 console.log(answer4);
@@ -64,56 +63,79 @@ console.log(answer5);
 
 answer5 = answer5.toLowerCase();
 
-if (answer5 === 'yes' || answer5 === 'n') {
-    alert('True. Wasn\'t my favorite though.');
-}   else if (answer5 === 'no' || answer5 === 'n') {
-    alert('Incorrect');
+if (answer5 === 'yes' || answer5 === 'y') {
+  alert('True. Wasn\'t my favorite though.');
+} else if (answer5 === 'no' || answer5 === 'n') {
+  alert('Incorrect');
 } else {
-    alert('My favorite thing to cook was pasta!');
+  alert('My favorite thing to cook was pasta!');
 }
 ///////////////////////////////////////////////////
-let numTurns = 0;
+// This is what I have so far. It runs an infinite loop. -___-
+// let numTurns = 0;
+// let instrumentAnswer = 4;
+// let answer6 = prompt('Okay, final question. How many instruments can I play? Numeric input only pls. ' + 'Attempt ' + numTurns);
 
+// while (answer6 !== instrumentAnswer && numTurns < 4) {
 
-let instrumentAnswer = 4;
-let answer6 = prompt('Turn ' + numTurns + ': Okay, final question. How many instruments can I play?');
+//   if (answer6 < instrumentAnswer) {
+//     alert ('That\'s too low a number.');
+//   }
+//   else if (answer6 > instrumentAnswer) {
+//     alert ('That\'s too high a number.');
+//   }
+//   else if (answer6 === instrumentAnswer) {
+//     alert('Yes! You got it right!');
+//   }
+//   else {
+//     alert('Is that even a number?');
+//   }
+// }
 
-while (answer6 != instrumentAnswer && numTurns < 4) {
-    numTurns++;
-    console.log(answer6);
-}
-
-    if (instrumentAnswer == answer6) {
-        alert('Yes! You got it right!');
-   }
-   else if (answer6 > instrumentAnswer) {
-        alert ('That\'s too high a number :(');
-    }
-    else if (answer6 < answer6) {
-        alert ('That\'s too low a number :(');
-    }
-    else {
-        alert('Is that even a number?');
-    }
 
 ///////////////////////////////////////////////////
-// I created a variable, numTurns as my counter. I guess I could also use i.
+// This code feels redundant. Revisiting in the AM.
+
 let colors = ['blue', 'green', 'red', 'gray', 'black', 'orange'];
 console.log(colors);
 
 let answer7 = prompt('Turn' + numTurns + ' : What are my favorite colors?');
-while (answer7 != colors() && numTurns <= 6) {
+while (answer7 !== colors && numTurns <= 6) {
     numTurns++;
-}
+    if (answer7 === 'blue' && numTurns <= 6) {
+        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
+    }
+    if (answer7 === 'green' && numTurns <= 6){
+        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
+    }
+    if (answer7 === 'red' && numTurns <= 6) {
+        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
+    }
+    if (answer7 === 'gray' && numTurns <= 6) {
+        alert('You got one! ' + 'you have' + numTurns + 'remaining.');
+    }
+    if (answer7 === 'black' && numTurns <= 6) {
+        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
+    }
+    if (answer7 === 'orange' && numTurns <= 6) {
+        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
+    }
+    else (answer7 !== colors && numTurns <= 6); {
+        alert('Try again!');
+    }
 console.log(answer7);
 
 answer7 = answer7.toLowerCase();
+console.log(colors);
 
 ///////////////////////////////////////////////////
 
 //Final score draft.
 
 let solutions = ['pizza', 'houston', 'Checking out new wine bars', 'yes', 'yes', '4'] ['blue', 'green', 'red', 'gray', 'black', 'orange'];
-let user = []
 let score = 0
 console.log(solutions);
+for (let i=0; i < solutions.length; i++) {
+    solutions = solutions[i];
+    console.log(solutions);
+}
