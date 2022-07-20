72c9df95e316e4d27f43cd0c2501f89b485fc609
Andra <94204279+asteele1493@users.noreply.github.com>
2022-07-19 21:54:04 -0700

    Added seventh question and worked on final q.

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,7 @@
+/*eslint-disable indent */
 'use strict';
+
+let userPoints = 0;
 
 let nickname = window.prompt('Hello! What\'s your name? ');
 window.alert('Welcome, ' + nickname + '!');
@@ -7,26 +10,31 @@ let answer1 = prompt('What\'s my favorite food?');
 console.log(answer1);
 
 answer1 = answer1.toLowerCase();
+console.log(answer1);
 
 if (answer1 === 'pizza') {
   window.alert('Heck yes I will literally eat pizza all day!');
+  userPoints++;
 } else if (answer1 === 'breakfast tacos') {
   window.alert('Papas con huevo is forever number 1 in my heart.');
+  userPoints++;
 } else {
-  window.alert('Nah brah, not a fan.');
+  window.alert('Sorry, not a fan.');
 }
 
 let answer2 = prompt('Where did I like living most?');
 console.log(answer2);
 
 answer2 = answer2.toLowerCase();
+console.log(answer2);
 
 if (answer2 === 'houston') {
-  alert('Correct! H-town vicious baybee!');
+  window.alert('Correct! H-town vicious baybee!');
+  userPoints++;
 } else if (answer2 === 'los angeles') {
-  alert('You don\'t know me at all.');
+  window.alert('You don\'t know me at all.');
 } else {
-  alert('I guess that\'s cool too.');
+  window.alert('I guess that\'s cool too.');
 }
 
 let answer3 = prompt('What do I like doing with my free time?');
@@ -35,23 +43,25 @@ console.log(answer3);
 answer3 = answer3.toLowerCase();
 console.log(answer3);
 
-answer3 = answer3.toLowerCase();
-
-if (answer3 === 'checking out new wine bars') {
+if (answer3 === 'going to wine bars') {
   alert('Absolutely! All about that natty life.');
+  userPoints++;
 } else if (answer3 === 'going to the beach.') {
   alert('I do love some beach time');
+  userPoints++;
 } else {
-  alert('Did you read my bio? :)');
+  alert('Incorrect');
 }
 
 let answer4 = prompt('Do I like tinned conservas?');
 console.log(answer4);
 
 answer4 = answer4.toLowerCase();
+console.log(answer4);
 
 if (answer4 === 'yes' || answer4 === 'y') {
   alert('It\'s my favorite food in the world!');
+  userPoints++;
 } else if (answer4 === 'no' || answer4 === 'n') {
   alert('Read my bio again pls');
 } else {
@@ -62,80 +72,67 @@ let answer5 = prompt('Was I ever a chef?');
 console.log(answer5);
 
 answer5 = answer5.toLowerCase();
+console.log(answer5);
 
 if (answer5 === 'yes' || answer5 === 'y') {
-  alert('True. Wasn\'t my favorite though.');
+  alert('Correct!');
+  userPoints++;
 } else if (answer5 === 'no' || answer5 === 'n') {
-  alert('Incorrect');
+  alert('Incorrect.');
 } else {
   alert('My favorite thing to cook was pasta!');
 }
 ///////////////////////////////////////////////////
-// This is what I have so far. It runs an infinite loop. -___-
+// This is what I have so far. It took a lot of incremental testing and debugging to make it run. Only issue (I think) I'm running
+//into now is making my alert happen when the number is guessed correctly.
+
 // let numTurns = 0;
-// let instrumentAnswer = 4;
-// let answer6 = prompt('Okay, final question. How many instruments can I play? Numeric input only pls. ' + 'Attempt ' + numTurns);
+// let correctAnswer = 4 ;
+// let answeredCorrectly = false;
 
-// while (answer6 !== instrumentAnswer && numTurns < 4) {
-
-//   if (answer6 < instrumentAnswer) {
-//     alert ('That\'s too low a number.');
+// while(numTurns <4 && !answeredCorrectly) {
+//   let answer6 = prompt('Okay, final question. How many instruments can I play? Numerical input only pls. ' + 'Attempt ' + numTurns);
+//   if(answer6 === correctAnswer) {
+//     window.alert('Yes!, correct!');
+//     userPoints++;
+//     answeredCorrectly = false;
+//   } else (answer6 !== correctAnswer); {
+//     window.alert('Incorrect! Try again.');
+//     answeredCorrectly = true;
 //   }
-//   else if (answer6 > instrumentAnswer) {
-//     alert ('That\'s too high a number.');
-//   }
-//   else if (answer6 === instrumentAnswer) {
-//     alert('Yes! You got it right!');
-//   }
-//   else {
-//     alert('Is that even a number?');
-//   }
+//   numTurns++;
 // }
 
+// ///////////////////////////////////////////////////
+// Add a 7th question that has multiple possible correct answers that are stored in an array. Give the user 6 attempts to guess the correct answer.
+// The guesses will end once the user guesses a correct answer or they run out of attempts.
+// Display all possible correct answers to the user
+// Consider using a loop of some sort for this question.
 
-///////////////////////////////////////////////////
-// This code feels redundant. Revisiting in the AM.
-
-let colors = ['blue', 'green', 'red', 'gray', 'black', 'orange'];
-console.log(colors);
-
-let answer7 = prompt('Turn' + numTurns + ' : What are my favorite colors?');
-while (answer7 !== colors && numTurns <= 6) {
-    numTurns++;
-    if (answer7 === 'blue' && numTurns <= 6) {
-        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
-    }
-    if (answer7 === 'green' && numTurns <= 6){
-        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
-    }
-    if (answer7 === 'red' && numTurns <= 6) {
-        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
-    }
-    if (answer7 === 'gray' && numTurns <= 6) {
-        alert('You got one! ' + 'you have' + numTurns + 'remaining.');
-    }
-    if (answer7 === 'black' && numTurns <= 6) {
-        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
-    }
-    if (answer7 === 'orange' && numTurns <= 6) {
-        alert('You got one! ' + 'you have' + numTurns + 'remaining.'); 
-    }
-    else (answer7 !== colors && numTurns <= 6); {
-        alert('Try again!');
-    }
+let numAttempts = 0;
+let possibleAnswers = ['USA', 'France', 'Israel', 'Italy'];
+possibleAnswers = possibleAnswers.toLowerCase();
+let answer7 = prompt('Guess which countries I have lived in! You\'ll only get six tries to get a correct answer. ' + 'Attempt ' + numAttempts).toLowerCase();
 console.log(answer7);
-
-answer7 = answer7.toLowerCase();
-console.log(colors);
+let answeredCorrectly = false; 
+  while (numAttempts < 6 && !answeredCorrectly) {
+  //let answer7 = prompt('Guess which countries I have lived in! You\'ll only get six tries to get a correct answer. ' + 'Attempt ' + numAttempts).toLowerCase();
+  //console.log(answer7);
+      for (let i = 0; i < 6; i++) {
+        console.log(possibleAnswers);
+        alert('You got one right!'); 
+        userPoints++;
+        answeredCorrectly = true; 
+    } if (answer7 !== possibleAnswers) {
+    answeredCorrectly = false;
+  }
+  numAttempts++;
+}
 
 ///////////////////////////////////////////////////
 
 //Final score draft.
 
-let solutions = ['pizza', 'houston', 'Checking out new wine bars', 'yes', 'yes', '4'] ['blue', 'green', 'red', 'gray', 'black', 'orange'];
-let score = 0
-console.log(solutions);
-for (let i=0; i < solutions.length; i++) {
-    solutions = solutions[i];
-    console.log(solutions);
-}
+//let correctSolutions = ['pizza', 'breakfast tacos', 'houston', 'going to wine bars', 'going to the beach', 
+//'yes', 'y', 'yes', 'y', 4, 'USA', 'France', 'Israel, 'Italy' ];
+alert('you have ' + userPoints + ' points.');
